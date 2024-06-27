ec7b18880f7b01edf184bdc4cc6f1231f168b2df
nitin telang <nitintelang110@gmail.com>
2024-06-27 17:00:41 +0530

    commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
             <div className='items grid2'>
               <div className='box'>
                 <h4>ADDRESS:</h4>
-                <p>198 West 21th Street, Suite 72, BULDHANA</p>
+                <p>198 West 21th Street, Suite 7225, BULDHANA</p>
               </div>
               <div className='box'>
                 <h4>EMAIL:</h4>
